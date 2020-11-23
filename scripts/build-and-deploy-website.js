5cf504f93c7908711ebaf3b594cb53f4bb4c218c
robot-vcs-sync <robot-vcs-sync@yandex-team.ru>
2020-11-24 02:09:37 +0300

    [a6d46fc] Merge pull request #16807 from frontend-fork/anyatyu.ISL-9196.deploy-gatsby
    ISL-9196: Деплой нового Лего сайта для документации
    
    Originally committed Tue Nov 24 01:08:06 2020 +0300 by anyatyu
    
    Note: mandatory check (NEED_CHECK) was skipped
    ref:4c0f951a8decc59ac1b58e4dcbed43a17f0a6dec

---
+++ scripts/build-and-deploy-website.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
+const { exec } = require('child_process');
+const { resolve } = require('path');
+const { promisify } = require('util');
+
+const execa = promisify(exec);
+const websiteDir = resolve(__dirname, '../docs-site');
+const YENV = process.env.YENV;
+
+async function build() {
+    // TODO: Залогировать все этапы выполнения.
+    const [install, build] = await Promise.all([
+        await execa('npm ci', { cwd: websiteDir }),
+        await execa('npm run build', { cwd: websiteDir }),
+    ]);
+
+    console.log(install.stderr || install.stdout);
+    console.log(build.stderr || build.stdout);
+}
+
+// TODO: В идеале не использовать static-uploader,
+// а использоать aws node api, чтобы очищать бакет
+// от артефакторв прошлых сборок.
+async function deploy() {
+    const { stderr, stdout } = await execa(`YENV={${YENV}} npm run deploy`, { cwd: websiteDir });
+    console.log(stderr || stdout);
+}
+
+async function main() {
+    await build();
+    await deploy();
+}
+
+main();
