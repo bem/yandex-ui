5b37cd7b25b4c9846bcc126dca4976dc6a9a1dfb
robot-vcs-sync <robot-vcs-sync@yandex-team.ru>
2021-01-13 22:30:36 +0300

    [491bac9] Merge pull request #18537 from frontend-fork/petrukhin.ISL-9542.fix-text-examples
    ISL-9542: Text.ts: пофиксить глобальные селекторы в примере
    
    Originally committed Wed Jan 13 21:57:08 2021 +0300 by petrukhin
    
    Note: mandatory check (NEED_CHECK) was skipped
    ref:9fa59b4d52605027e9328a347e1e27f2b50af8b7

--- src/Text/Text.examples/desktop/common.examples.tsx
+++ src/Text/Text.examples/desktop/common.examples.tsx
@@ -22,60 +22,27 @@ const Unit: FC<Partial<TextProps> & { groupId?: string }> = ({
     as,
     ...props
 }) => {
-    const weightSelect = select(
-        'Weight',
-        [
-            'light',
-            'regular',
-            'medium',
-            'bold',
-        ],
-        weight,
+    const weightSelect = select('Weight', ['light', 'regular', 'medium', 'bold'], weight, groupId);
+    const typographySelect = select<Required<TextProps>['typography']>(
+        'Typography',
+        typographyValues,
+        typography!,
         groupId,
     );
-    const typographySelect = select<Required<TextProps>['typography']>(
-        'Typography', typographyValues, typography!, groupId,
-    );
 
-    return (
-        <Text
-            as={as}
-            className={className}
-            typography={typographySelect}
-            weight={weightSelect}
-            {...props}
-        />
-    );
+    return <Text as={as} className={className} typography={typographySelect} weight={weightSelect} {...props} />;
 };
 
 const Headline1 = (props: Partial<TextProps>) => (
-    <Unit
-        as="h1"
-        typography="display-xl"
-        weight="light"
-        groupId="Display"
-        { ...props }
-    />
+    <Unit as="h1" typography="display-xl" weight="light" groupId="Display" {...props} />
 );
 
 const Headline2 = (props: Partial<TextProps>) => (
-    <Unit
-        as="h2"
-        typography="headline-xl"
-        weight="medium"
-        groupId="Headline"
-        {...props}
-    />
+    <Unit as="h2" typography="headline-xl" weight="medium" groupId="Headline" {...props} />
 );
 
 const Headline3 = (props: Partial<TextProps>) => (
-    <Unit
-        as="h3"
-        typography="subheader-xl"
-        weight="medium"
-        groupId="Subheader"
-        { ...props }
-    />
+    <Unit as="h3" typography="subheader-xl" weight="medium" groupId="Subheader" {...props} />
 );
 
 const Paragraph = ({ typography = 'body-long-xl', ...props }: Partial<TextProps>) => (
@@ -84,40 +51,29 @@ const Paragraph = ({ typography = 'body-long-xl', ...props }: Partial<TextProps>
         typography={typography}
         weight="regular"
         groupId={typography === 'body-long-xl' ? 'Paragraph Long' : 'Paragrapth Short'}
-        { ...props }
+        {...props}
     />
 );
 
 const Caption = (props: Partial<TextProps>) => (
-    <Unit
-        as="p"
-        typography="caption-l"
-        weight="regular"
-        groupId="Caption"
-        { ...props }
-    />
+    <Unit as="p" typography="caption-l" weight="regular" groupId="Caption" {...props} />
 );
 
 const Overline = (props: Partial<TextProps>) => (
-    <Unit
-        as="p"
-        typography="overline-s"
-        weight="medium"
-        groupId="Overline"
-        {...props}
-    />
+    <Unit as="p" typography="overline-s" weight="medium" groupId="Overline" {...props} />
 );
 
 export const Common = () => {
     const preset = select('theme-preset', presetsKeys, 'default');
     return (
-        <section className={cnTheme(presets[preset])} style={{ backgroundColor: 'var(--color-bg-default)' }}>
+        <section
+            className={[cnTheme(presets[preset]), 'text-common-example'].join(' ')}
+            style={{ backgroundColor: 'var(--color-bg-default)' }}
+        >
             <Headline1 className="display">
                 Миссия Яндекса — помогать людям решать задачи и достигать своих целей в жизни.
             </Headline1>
-            <Headline2 className="headline">
-                Счастье пользователей
-            </Headline2>
+            <Headline2 className="headline">Счастье пользователей</Headline2>
             <Paragraph className="body-long">
                 Все сервисы Яндекса сделаны для того, чтобы улучшать и упрощать жизнь людей. Каждый из наших сервисов
                 помогает делать что-то, что ещё вчера казалось невозможным. Несколько лет назад мы не могли представить
@@ -141,22 +97,17 @@ export const Common = () => {
                 наших сервисах. Любое изменение должно быть обосновано — это помогает нам понять, действительно ли оно
                 полезно, действительно ли хорошо для пользователей.
             </Paragraph>
-            <Headline3 className="subheader">
-                1. Мы работаем для пользователей
-            </Headline3>
+            <Headline3 className="subheader">1. Мы работаем для пользователей</Headline3>
             <Paragraph typography="body-short-xl" style={{ columnCount: 2 }} className="body-short">
-                Наша главная цель — создавать качественные сервисы для миллионов пользователей интернета, и это
-                отражено во внутренних документах Яндекса. Важно, чтобы к достижению этой цели стремился каждый
-                член команды Яндекс ожидает, что члены команды будут чувствовать ответственность не только за свою
-                часть работы, но и за продукт в целом. Будьте неравнодушны к отзывам пользователей, предлагайте
-                усовершенствования.
+                Наша главная цель — создавать качественные сервисы для миллионов пользователей интернета, и это отражено
+                во внутренних документах Яндекса. Важно, чтобы к достижению этой цели стремился каждый член команды
+                Яндекс ожидает, что члены команды будут чувствовать ответственность не только за свою часть работы, но и
+                за продукт в целом. Будьте неравнодушны к отзывам пользователей, предлагайте усовершенствования.
             </Paragraph>
             <ul>
                 <li>
                     <img src={maps} alt="" />
-                    <Headline3 className="subheader">
-                        Выход в город
-                    </Headline3>
+                    <Headline3 className="subheader">Выход в город</Headline3>
                     <Paragraph typography="body-short-xl" overflow="ellipsis" maxLines={2} className="body-short">
                         Как выглядели бы карты метро, если бы станции назывались в честь музеев, парков, гостиниц,
                         больниц и других мест, которые притягивают людей на эти станции.
@@ -170,35 +121,23 @@ export const Common = () => {
                 </li>
                 <li>
                     <img src={pin} alt="" />
-                    <Headline3>
-                        Выход в город
-                    </Headline3>
+                    <Headline3>Выход в город</Headline3>
                     <Paragraph typography="body-short-xl" overflow="fade" maxLines={2}>
                         Как выглядели бы карты метро, если бы станции назывались в честь музеев, парков, гостиниц,
                         больниц и других мест, которые притягивают людей на эти станции.
                     </Paragraph>
-                    <Caption color="secondary">
-                        Москва • Санкт-Петербург • Минск
-                    </Caption>
-                    <Overline color="secondary">
-                        ИССЛЕДОВАНИЯ • 14 МАЙ 2019
-                    </Overline>
+                    <Caption color="secondary">Москва • Санкт-Петербург • Минск</Caption>
+                    <Overline color="secondary">ИССЛЕДОВАНИЯ • 14 МАЙ 2019</Overline>
                 </li>
                 <li>
                     <img src={stat} alt="" />
-                    <Headline3>
-                        Выход в город
-                    </Headline3>
+                    <Headline3>Выход в город</Headline3>
                     <Paragraph typography="body-short-xl" overflow="fade-horizontal" maxLines={2}>
                         Как выглядели бы карты метро, если бы станции назывались в честь музеев, парков, гостиниц,
                         больниц и других мест, которые притягивают людей на эти станции.
                     </Paragraph>
-                    <Caption color="secondary">
-                        Москва • Санкт-Петербург • Минск
-                    </Caption>
-                    <Overline color="secondary">
-                        ИССЛЕДОВАНИЯ • 14 МАЙ 2019
-                    </Overline>
+                    <Caption color="secondary">Москва • Санкт-Петербург • Минск</Caption>
+                    <Overline color="secondary">ИССЛЕДОВАНИЯ • 14 МАЙ 2019</Overline>
                 </li>
             </ul>
         </section>
