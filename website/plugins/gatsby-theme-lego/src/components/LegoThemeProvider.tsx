0911d0ac71d7f7d2117bb0ed8dc6164a1b0fa0fa
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2021-05-11 21:12:13 +0300

    sync upstream ref:f6b9ce59bcec3855cacc458b386959e0936e0567

--- website/plugins/gatsby-theme-lego/src/components/LegoThemeProvider.tsx
+++ website/plugins/gatsby-theme-lego/src/components/LegoThemeProvider.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
-import { cnTheme } from '@yandex-lego/components/Theme';
+import { cnTheme, configureRootTheme } from '@yandex-lego/components/Theme';
 import { theme } from '@yandex-lego/components/Theme/presets/default';
+
+configureRootTheme({ theme });
 
 export const LegoThemeProvider: FC = (props) => {
     const { children } = props;
